46a50bfa565410f84d2b89598bf5a0d6a8d4c88e
Hugo Pinheiro <hugo_hp10@hotmail.com>
2023-04-11 22:25:39 -0300

    Initial Commit

---
+++ GloboPlay Teste/GloboPlay_TesteApp.swift
@@ -0,0 +1,17 @@
+//
+//  GloboPlay_TesteApp.swift
+//  GloboPlay Teste
+//
+//  Created by HP on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct GloboPlay_TesteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
